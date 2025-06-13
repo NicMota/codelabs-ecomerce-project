7e6c57f3f336a119f1270e850cfb18bfedb95ee9
NicMota <nicjmota3@gmail.com>
2025-06-13 10:18:37 -0300

    start products fetch in front

--- front/src/pages/Store.tsx
+++ front/src/pages/Store.tsx
@@ -1,31 +1,27 @@
-import { useEffect, useState } from "react"
 import type { Product } from "../types";
 import { ProductCard } from "../components/StoreComponents";
+import axios from "axios";
 
 export default function Store()
 {   
-    const [products,setProducts] = useState<Product[]>([]);
+ 
+    const { data: products, isLoading} = useQuery(["products"], async () => {
+        const res = await axios.get("http://localhost:8082/products");
+        return res.data;
+    });
 
-    useEffect(()=>
-    {
-         setProducts([
-            {
-                name: "Notebook", description: "Lightweight laptop", price: 2999,
-                id: ""
-            },
-            {
-                name: "Headphones", description: "Noise cancelling", price: 499,
-                id: ""
-            },
-        ])
-    },[]);
+ 
     return(
         <div className=" h-fit  p-4 m-10 flex  gap-x-4 gap-y-8 ">
-            {products.map((product: Product,index) => (
+            {products.map((product: Product, index : number) => (
               <ProductCard key={index} product={product}/>
             ))
             }
         
         </div>
     )
+}
+
+function useQuery(arg0: string[], arg1: () => Promise<any>): { data: any; isLoading: any; } {
+    throw new Error("Function not implemented.");
 }
